a1b9cfd40a0abb5394a7cea8e322617dc2e120a7
shivasrk12345 <jagdish.saini@teamcomputers.com>
2021-05-10 21:05:54 +0530

    api test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const express = require('express')
 const app = express()
-const port = 443
+var port = process.env.PORT || 3000;
 
 var nowDate = new Date(); 
 var month = (nowDate.getMonth()+1)
@@ -60,10 +60,8 @@ function api_call(){
 setInterval(api_call, 60000);
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/', function(req, res){
+    res.send("Hello world!");
+ });
+ 
+ app.listen(port);
